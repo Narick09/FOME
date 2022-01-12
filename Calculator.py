cdea937742dad2605b313d739b5d0d3e6b3640bf
void <49158033+Narick09@users.noreply.github.com>
2022-01-12 18:37:44 +0700

    Add files via upload
    нерабочая версия поля Земли. Предыдущая версия вроде работала, но, скорее всего, как-то не так :/ (путаница со знаками в поле)

--- Calculator.py
+++ Calculator.py
@@ -1,14 +1,15 @@
 from math import *
 import numpy as np
 
-def eqSolut(U, m, xCoordinate_0, speed_0, total_time, step_t):
+def eqSolut(F, m, xCoordinate_0, speed_0, total_time, step_t):
     x = [xCoordinate_0]#x0
     v = []
     t = [0.0]
 
-    dx = 1e-10  # сколько тут надо взять???
+    #dx = 1e-10  # сколько тут надо взять???
 
-    a0 = -1.0 * (U(xCoordinate_0 + dx) - U(xCoordinate_0)) / dx / m #заменить потенциал на силу
+    #a0 = -1.0 * (U(xCoordinate_0 + dx) - U(xCoordinate_0)) / dx / m #заменить потенциал на силу
+    a0 = F(xCoordinate_0) / m  # заменить потенциал на силу
     v_12 = speed_0 + a0 * step_t / 2 #v_12
     v.append(v_12)
     # E = m * speed_0 ** 2 / 2 + U(xCoordinate_0)
@@ -34,12 +35,12 @@ def eqSolut(U, m, xCoordinate_0, speed_0, total_time, step_t):
 
     for i in range (1, N):
         x_pr = x[-1]
-        #print(x_pr)
-        x.append(x_pr + v[-1] * step_t)
-        a0 = -1.0 * (U(x[-1] + dx) - U(x[-1])) / dx / m #-1.0 * (U(x[-1]) - U(x_pr)) / (x[-1] - x_pr) / m
+        a0 = F(x[-1]) / m
+        #a0 = -1.0 * (U(x[-1] + dx) - U(x[-1])) / dx / m #-1.0 * (U(x[-1]) - U(x_pr)) / (x[-1] - x_pr) / m
         v.append(v[-1] + a0 * step_t)#v_(n+1/2)
         t.append(i * step_t)
+        #print(x_pr)
+        x.append(x_pr + v[-1] * step_t)
 #    v.append(v[-1])
     print(t[-1])
     return t, x, v
-
--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import Calculator as calc
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 
-r0 = -2
-vr_0 = -5
+r0 = 200000#-2
+vr_0 = -2
 total_time = 5
-N = 100000
 m = 1.67e-27
-step_t = total_time / N
+step_t = 1e-4
 min_border = -40
 max_border = 40
-R = 6371 #Earth radius
-
+R = 1e-2 #мат точка
+#R = 6371 #Earth radius
+G = 6.673e-11  # C
+M = 5.9742e24  # C
 
 def potent_oscillator(x):
     w = 3
@@ -33,27 +34,31 @@ def gravitational_potential(x):
 
 
 def gravitational_planet_potential(x):
-    G = 6.673e-11 #C
-    M = 5.9742e24 #C
     global min_border
     min_border = 1
     global max_border
     max_border = r0 + 1000
-    if abs(x - R) < 10:
-        return -m * M * G / (R + 10)
-    if x < R:
-        return 5e-20
+    # if abs(x - R) < 10:
+    #     return -m * M * G / (R + 10)
+    if abs(x) < R:
+        return -m * M * G / R
     return -m * M * G / x
 
 
 def U_(x):
-    return potent_oscillator(x)
+    return gravitational_planet_potential(x)
 
+def gravitational_planet_force(x):
+    if abs(x) < R:
+        return -m * M * G / (R**2)
+    return -m * M * G / (x**2)
+
+def Force(x):
+    return gravitational_planet_force(x)
 
 def update(val):
     total_time = val
-    step_t = total_time / N
-    t, r, v = calc.eqSolut(U_, m, r0, vr_0, total_time, step_t)
+    t, r, v = calc.eqSolut(Force, m, r0, vr_0, total_time, step_t)
 
     E_k_tmp = [(v[i]) ** 2 * m / 2 + U_(r[i - 1]) for i in range(1, len(r) - 1)]
     E_k_tmp.append(E_k_tmp[-1])
@@ -74,7 +79,7 @@ def update(val):
     fg.canvas.draw_idle()
 
 
-t, r, v = calc.eqSolut(U_, m, r0, vr_0, total_time, step_t)
+t, r, v = calc.eqSolut(Force, m, r0, vr_0, total_time, step_t)
 fg = plt.figure(figsize=(12, 7))
 plt.subplot(221)
 line_tr, = plt.plot(t, r, label='r')
@@ -102,9 +107,11 @@ E_k_tmp = [(v[i]) ** 2 * m / 2 + U_(r[i - 1]) for i in range(1, len(r) - 1)]
 E_k_tmp.append(E_k_tmp[-1])
 
 E_k_tmp.append(E_k_tmp[-1])
-x_arr = [(i * 0.1) for i in range(-40, 40)]
+# x_arr = [(i * 0.1) for i in range(-40, 40)]
+# U_arr = [U_(i*0.1) for i in range(-40, 40)]
 
-U_arr = [U_(i*0.1) for i in range(-40, 40)]
+x_arr = [i for i in range(1, r0 + 1000)]
+U_arr = [U_(i) for i in range(1, r0 + 1000)]
 
 plt.subplot(224)
 plt.plot(x_arr, U_arr, label='U')
