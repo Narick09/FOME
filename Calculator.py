e2fc62ca8da03e97a679b9be17b3b157e26ad1dd
void <49158033+Narick09@users.noreply.github.com>
2022-01-13 14:18:39 +0700

    Add files via upload
    подготовочка к векторам

--- Calculator.py
+++ Calculator.py
@@ -2,7 +2,7 @@ from math import *
 import numpy as np
 
 
-def accelerate_through_potential(U, xCoordinate_0, dx, m):
+def accelerate_through_potential(U, xCoordinate_0, dx, m):  # пока только в одномерье!!!
     return -1.0 * (U(xCoordinate_0 + dx) - U(xCoordinate_0)) / dx / m
 
 
@@ -11,7 +11,7 @@ def accelerate_through_force(force, xCoordinate_0, dx, m):
 
 
 def eqSolut(func, m, xCoordinate_0, speed_0, total_time, step_t, mode=False):
-    x = [xCoordinate_0]#x0
+    x = [xCoordinate_0]  # x0
     v = []
     t = [0.0]
 
@@ -21,23 +21,18 @@ def eqSolut(func, m, xCoordinate_0, speed_0, total_time, step_t, mode=False):
 
     dx = 1e-10  # сколько тут надо взять???
 
-    #a0 = -1.0 * (U(xCoordinate_0 + dx) - U(xCoordinate_0)) / dx / m #заменить потенциал на силу
-    #a0 = force(xCoordinate_0) / m
     a0 = accelerate(func, xCoordinate_0, dx, m)
-    v_12 = speed_0 + a0 * step_t / 2 #v_12
+    v_12 = speed_0 + a0 * step_t / 2  # v_12
     v.append(v_12)
 
     N = int(total_time / step_t)
 
     for i in range (1, N):
         x_pr = x[-1]
-        #print(x_pr)
-        x.append(x_pr + v[-1] * step_t)
-        #a0 = -1.0 * (U(x[-1] + dx) - U(x[-1])) / dx / m #-1.0 * (U(x[-1]) - U(x_pr)) / (x[-1] - x_pr) / m
         a0 = accelerate(func, x[-1], dx, m)
-        v.append(v[-1] + a0 * step_t)#v_(n+1/2)
+        v.append(v[-1] + a0 * step_t)  # v_(n+1/2)
         t.append(i * step_t)
-#    v.append(v[-1])
-    print(t[-1])
+        x.append(x_pr + v[-1] * step_t)
+    # print(t[-1])
     return t, x, v
 
--- main.py
+++ main.py
@@ -2,34 +2,33 @@ import math
 import Calculator as calc
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
+# how to Draw 2D U_?
 
-r0 = 200000
-vr_0 = 2
-total_time = 5
-m = 1.67e-27
-step_t = 1e-4
-min_border = -4
-max_border = 4
-R = 6371 #Earth radius
+# constants:
+m = 200  # 1.67e-27
+R = 6371  # Earth radius
 G = 6.673e-11  # C
 M = 5.9742e24  # C
 
+step_t = 1e-4
+total_time = 5
+# graph range
+min_border = -4
+max_border = 4
+koef = 0  # для отображения графика
+
+r0 = 200000
+vr_0 = 2
+# r0 = [200000, 200000]
+# vr_0 = [-2, 0]
+
+
 def potent_oscillator(x):
     w = 3
-    global min_border
-    min_border = -4
-    global max_border
-    max_border = 4
     return m * w**2 * x ** 2 / 2
 
 
 def gravitational_planet_potential(x):
-    global min_border
-    min_border = 1
-    global max_border
-    max_border = R * 100
-    if abs(x - R) < 10:
-        return -m * M * G / (R + 10)
     if x < R:
         return 5e-20
     return -m * M * G / x
@@ -41,34 +40,66 @@ def force_oscillator(x):
 
 
 def gravitational_planet_force(x):
-    global min_border
-    min_border = 1
-    global max_border
-    max_border = R * 100
     return -m * M * G / x**2
 
 
-def U_(x):
-    return gravitational_planet_potential(x)
+def gravitational_planet_potential_2D(radius):  # radius = sqrt(x**2 + y**2)
+    # if abs(x - R) < 10:
+    #     return -m * M * G / (R + 10)
+    # if abs(y - R) < 10:
+    #     return -m * M * G / (R + 10)
+    # if x < R:
+    #     return 5e-20
+    # if y < R:
+    #     return 5e-20
+    if radius < R:
+        return 5e-20
+    return -m * M * G / radius
 
 
-def Force(x):
-    return gravitational_planet_force(x)
+def gravitational_planet_force_2D(r_vec):
+    return [-m * M * G * 2 * r_vec[0] / (r_vec[0]**2 + r_vec[1]**2)**(3.0/2),
+            -m * M * G * 2 * r_vec[1] / (r_vec[0]**2 + r_vec[1]**2)**(3.0/2)]
 
 
-#func_ = [U_, False]
-func_ = [Force, True]
+global U_
+global Force
+
+
+def set_params(mode=True):
+    global min_border
+    global max_border
+    global koef
+    global r0
+    global U_
+    global Force
+
+    if mode:
+        min_border = 1
+        max_border = R * 100
+        koef = 1
+        r0 = 200000
+        U_ = gravitational_planet_potential
+        Force = gravitational_planet_force
+    else:
+        min_border = -4
+        max_border = 4
+        koef = 10
+        r0 = -2
+        U_ = potent_oscillator
+        Force = force_oscillator
+
 
 def update(val):
     total_time = val
     t, r, v = calc.eqSolut(func_[0], m, r0, vr_0, total_time, step_t, func_[1])
+    # uncomment all it
+    # E_k_tmp = [(v[i]) ** 2 * m / 2 + U_(r[i - 1]) for i in range(1, len(r) - 1)]
+    # E_k_tmp.append(E_k_tmp[-1])
+    # E_k_tmp.append(E_k_tmp[-1])
 
-    E_k_tmp = [(v[i]) ** 2 * m / 2 + U_(r[i - 1]) for i in range(1, len(r) - 1)]
-    E_k_tmp.append(E_k_tmp[-1])
-    E_k_tmp.append(E_k_tmp[-1])
-
-    line_rv.set_xdata(r)
-    line_rv.set_ydata(v)
+    # line_rv.set_xdata(r)#change to _xy
+    # line_rv.set_ydata(v)
 
     line_tv.set_xdata(t)
     line_tv.set_ydata(v)
@@ -76,51 +107,58 @@ def update(val):
     line_tr.set_xdata(t)
     line_tr.set_ydata(r)
 
-    line_rE.set_xdata(r)
-    line_rE.set_ydata(E_k_tmp)
+    # line_rE.set_xdata(r)
+    # line_rE.set_ydata(E_k_tmp)
 
     fg.canvas.draw_idle()
 
 
+set_params(True)  # true - Earth, false - oscillator
+# func_ = [U_, False]
+func_ = [Force, True]
 t, r, v = calc.eqSolut(func_[0], m, r0, vr_0, total_time, step_t, func_[1])
+
+
+# r_x = [r[i][0] for i in range(0, len(r))]
+# r_y = [r[i][1] for i in range(0, len(r))]
+
 fg = plt.figure(figsize=(12, 7))
 plt.subplot(221)
 line_tr, = plt.plot(t, r, label='r')
-plt.title('Coordinate')
+plt.title('Coordinate_x')
 plt.xlabel('t')
 plt.ylabel('r')
 plt.grid()
 
 plt.subplot(222)
 line_tv, = plt.plot(t, v, label='v')
-plt.title('Speed')
+plt.title('Speed_ x')
 plt.xlabel('t')
 plt.ylabel('v')
 plt.grid()
 
-plt.subplot(223)
-line_rv, = plt.plot(r, v, label='v_rad(x)')
-plt.title('V (x)')
-plt.xlabel('r')
-plt.ylabel('v_rad')
-plt.grid()
+# plt.subplot(223)
+# line_rv, = plt.plot(r_x, r_y, label='Y(x)')
+# plt.title('y(x)')
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.grid()
 
-#    E_k_tmp = [((x[i] - x[i-1]) / step_t)**2 * m / 2 + U_(x[i - 1]) for i in range(1, len(x))]
-E_k_tmp = [(v[i]) ** 2 * m / 2 + U_(r[i - 1]) for i in range(1, len(r) - 1)]
-E_k_tmp.append(E_k_tmp[-1])
+# ucomment:
+# E_k_tmp = [(v[i]) ** 2 * m / 2 + U_(r[i - 1]) for i in range(1, len(r) - 1)]
+# E_k_tmp.append(E_k_tmp[-1])
+#
+# E_k_tmp.append(E_k_tmp[-1])
+# x_arr = [(i / koef) for i in range(min_border * koef, max_border * koef)]
+# U_arr = [U_(i / koef) for i in range(min_border * koef, max_border * koef)]
 
-E_k_tmp.append(E_k_tmp[-1])
-koef = 1
-x_arr = [(i / koef) for i in range(min_border * koef, max_border * koef)]
-U_arr = [U_(i / koef) for i in range(min_border * koef, max_border * koef)]
-
-plt.subplot(224)
-plt.plot(x_arr, U_arr, label='U')
-line_rE, = plt.plot(r, E_k_tmp, 'r-.', label='particle Energy')
-plt.title('Energy')
-plt.xlabel('r')
-plt.ylabel('E')
-plt.grid()
+# plt.subplot(224)
+# plt.plot(x_arr, U_arr, label='U')
+# line_rE, = plt.plot(r, E_k_tmp, 'r-.', label='particle Energy')
+# plt.title('Energy')
+# plt.xlabel('r')
+# plt.ylabel('E')
+# plt.grid()
 
 ax_total_time = plt.axes([0.1, 0.025, 0.8, 0.0125])
 total_time_slider = Slider(
