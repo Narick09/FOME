b5bafe0fd5ab0d753a6f4b27aece4e1231ec7067
void <49158033+Narick09@users.noreply.github.com>
2022-01-14 05:06:23 +0700

    Add files via upload
    Окончательная версия (вроде) с работающим подсчетом в поле тяготения и осциллятора. С работающим слайдером и выводом эффективной энергии

--- main.py
+++ main.py
@@ -3,25 +3,29 @@ import numpy as np
 import Calculator as calc
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
-# how to Draw 2D U_?
+
+
+# true - Earth, false - oscillator - in set_params
+Earth = True
+Oscillator = False
+
+choose_field = Oscillator  # тут выбирать, что рисовать - гравитационное, или осциллятор
+# настройка параметров ниже - в функции set_params
 
 # constants:
-m = 1000  # 1.67e-27
 R = 6371000.0  # Earth radius
 G = 6.673e-11  # C
 M = 5.9742e24  # C
 
 global step_t
 global total_time
-# graph range - задается в set_params
-global min_border
-global max_border
-global koef  # для отображения графика
+
+global m
 global w
 
 global r0
 global vr_0
-# global r_norm # к оптимизации работы программы - сразу заполнять этот массив по хожу счета
+# global r_norm # к оптимизации работы программы - сразу заполнять этот массив по ходу счета
 
 
 def trans(x, y):
@@ -53,24 +57,10 @@ def trans(x, y):
     return r_temp, hi
 
 
-# def potent_oscillator(x):
-#     w = 3
-#     return m * w**2 * x ** 2 / 2
-#
-#
 def gravitational_planet_potential(x):
     if x < R:
         return 5e-20
     return -m * M * G / x
-#
-#
-# def force_oscillator(x):
-#     w = 3
-#     return -m * w**2 * x
-#
-#
-# def gravitational_planet_force(x):
-#     return -m * M * G / x**2
 
 
 def gravitational_planet_force_2D(r_vec):
@@ -83,22 +73,20 @@ def gravitational_planet_force_2D(r_vec):
                      A * np.sin(arr_hi) / arr_r**2])
 
 
-def potent_oscillator_2D(r_vec):
-    return np.dot(w, m * r_vec ** 2 / 2)
+def potent_oscillator(r_mod):
+    return m * w * r_mod ** 2 / 2
 
 
 def oscillator_force_2D(r_vec):
-    return r_vec
+    return np.array([-m * w * r_vec[0], -m * w * r_vec[1]])
 
 
 # global U_
 global Force
 
 
-def set_params(mode=True):
-    global min_border
-    global max_border
-    global koef
+def set_params(mode=Earth):
+    global m
     global r0
     global vr_0
     global U_
@@ -107,26 +95,23 @@ def set_params(mode=True):
     global total_time
     global step_t
 
-    if mode:  # здесь настраиваются начальные параметры для 2д случая
+    if mode:  # здесь настраиваются начальные параметры для 2д случая гравитационного поля
+        m = 1000
         vr_0 = np.array([-3000.0, 0.0])
         total_time = 512700
         step_t = 100
-        min_border = 1
-        max_border = R * 100
-        koef = 1
         r0 = np.array([R + 2 * R, R + 3 * R])
         U_ = gravitational_planet_potential
         Force = gravitational_planet_force_2D
-    else:
-        vr_0 = np.array([-2, 0])
-        w = np.array([3**2, 3**2])
+    else:  # здесь настраиваются начальные параметры для 2д случая поля осциллятора
+        m = 1.67e-27
+        vr_0 = np.array([-2.0, 0.0])
+        w0 = 3
+        w = w0**2
         total_time = 5
         step_t = 1e-3
-        # min_border = -4
-        # max_border = 4
-        # koef = 10
-        r0 = np.array([-2, 0])
-        # U_ = potent_oscillator_2D
+        r0 = np.array([2.0, 1.0])
+        U_ = potent_oscillator
         Force = oscillator_force_2D
 
 
@@ -137,10 +122,15 @@ def update(val):
     v_x = [v[i][0] for i in range(0, len(v))]
     v_y = [v[i][1] for i in range(0, len(v))]
 
-    # uncomment all it
-    # E_k_tmp = [(v[i]) ** 2 * m / 2 + U_(r[i - 1]) for i in range(1, len(r) - 1)]
-    # E_k_tmp.append(E_k_tmp[-1])
-    # E_k_tmp.append(E_k_tmp[-1])
+    r_norm = [np.linalg.norm(r[i]) for i in range(0, len(r))]
+    v_mod, fi_in_v = [trans(v_x[i], v_y[i])[0] for i in range(0, len(v))], \
+                     [trans(v_x[i], v_y[i])[1] for i in range(0, len(v))]
+    E_full = [(v_mod[i]) ** 2 * m / 2 + U_(r_norm[i]) for i in range(0, len(r_norm))]
+
+    v_fi = [v_mod[i] * np.sqrt(abs(1 - (np.dot(v[i], r[i]) / np.linalg.norm(v[i]) / np.linalg.norm(r[i])) ** 2)) for i in range(0, len(v_mod))]  # костыль здесь!
+
+    x_eff = [(np.linalg.norm(r[i])) for i in range(0, len(v_fi))]
+    U_eff = [U_(x_eff[i]) + v_fi[i] ** 2 * m / 2 for i in range(0, len(v_fi))]
 
     line_xy.set_xdata(r_x)
     line_xy.set_ydata(r_y)
@@ -151,14 +141,16 @@ def update(val):
     line_tr.set_xdata(t)
     line_tr.set_ydata(r_x)
 
-    # line_rE.set_xdata(r)
-    # line_rE.set_ydata(E_k_tmp)
+    line_rE.set_xdata(r_norm)
+    line_rE.set_ydata(E_full)
+
+    line_Uef.set_xdata(x_eff)
+    line_Uef.set_ydata(U_eff)
 
     fg.canvas.draw_idle()
 
-
 # init
-set_params(True)  # true - Earth, false - oscillator
+set_params(choose_field)  # true - Earth, false - oscillator
 # func_ = [U_, False]
 func_ = [Force, True]
 
@@ -185,11 +177,11 @@ plt.ylabel('v')
 plt.grid()
 
 plt.subplot(223)
-circle1 = plt.Circle((0, 0), R, color='r', fill=True)
+if choose_field:
+    circle1 = plt.Circle((0, 0), R, color='r', fill=True)
+    ax = plt.gca()
+    ax.add_patch(circle1)
 line_xy, = plt.plot(r_x, r_y, label='Y(x)')
-ax=plt.gca()
-ax.add_patch(circle1)
-#plt.axis('scaled')
 plt.title('y(x)')
 plt.xlabel('x')
 plt.ylabel('y')
@@ -197,20 +189,18 @@ plt.grid()
 
 
 r_norm = [np.linalg.norm(r[i]) for i in range(0, len(r))]
-v_mod, fi_in_v = [trans(v[i][0], v[i][1])[0] for i in range(0, len(v))], \
-            [trans(v[i][0], v[i][1])[1] for i in range(0, len(v))]
+v_mod, fi_in_v = [trans(v_x[i], v_y[i])[0] for i in range(0, len(v))], \
+                 [trans(v_x[i], v_y[i])[1] for i in range(0, len(v))]
 E_full = [(v_mod[i]) ** 2 * m / 2 + U_(r_norm[i]) for i in range(0, len(r_norm))]
 
-v_fi = [v_mod[i] * np.sqrt(1 - (np.dot(v[i], r[i]) / np.linalg.norm(v[i]) / np.linalg.norm(r[i]))**2) \
-        for i in range(0, len(v_mod))]
-# x_arr = [(i / koef) for i in range(int(min_border * koef), int(max_border * koef), int(step_t))]
-# U_arr = [U_(i / koef) for i in range(int(min_border * koef), int(max_border * koef), int(step_t))]
+v_fi = [v_mod[i] * np.sqrt(abs(1 - (np.dot(v[i], r[i]) / np.linalg.norm(v[i]) / np.linalg.norm(r[i]))**2)) for i in range(0, len(v_mod))]  # костыль здесь!
+
 
 x_eff = [(np.linalg.norm(r[i])) for i in range(0, len(v_fi))]
 U_eff = [U_(x_eff[i]) + v_fi[i]**2 * m / 2 for i in range(0, len(v_fi))]
 
 plt.subplot(224)
-plt.plot(x_eff, U_eff, label='U')
+line_Uef, = plt.plot(x_eff, U_eff, label='U')
 line_rE, = plt.plot(r_norm, E_full, 'r-.', label='particle Energy')
 plt.title('Energy')
 plt.xlabel('r')
